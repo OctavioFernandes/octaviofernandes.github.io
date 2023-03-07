2ea5ef58ec77d2dec15ebba2f6ae1a7b08e43caa
OctavioFernandes <octaviomgfernandes@gmail.com>
2023-03-07 18:49:30 +0000

    firebase teste 2

---
+++ firebase.js
@@ -0,0 +1,39 @@
+(function () {
+
+    // Import the functions you need from the SDKs you need
+    // import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js";
+    // import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-analytics.js";
+    // TODO: Add SDKs for Firebase products that you want to use
+    // https://firebase.google.com/docs/web/setup#available-libraries
+
+    // Your web app's Firebase configuration
+    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+    const firebaseConfig = {
+        apiKey: "AIzaSyBM6QeVOOFtLBdRoj3og1leAmL36gqIHR0",
+        authDomain: "testebd-b3fee.firebaseapp.com",
+        databaseURL: "https://testebd-b3fee-default-rtdb.europe-west1.firebasedatabase.app",
+        projectId: "testebd-b3fee",
+        storageBucket: "testebd-b3fee.appspot.com",
+        messagingSenderId: "1077604731878",
+        appId: "1:1077604731878:web:1ba4ee90f0ee9a497b6427",
+        measurementId: "G-GNQ9WTWDRZ"
+    };
+
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+    // const analytics = getAnalytics(app);
+})()
+
+function PostBd() {
+
+    console.log(document.getElementById("texto").value);
+
+    var id = firebase.database().ref().child('novo').push().key;
+    var update = {};
+    update['/novo/' + id] = document.getElementById("texto").value;
+    firebase.database().ref().update(update);
+
+    document.getElementById("texto").value = '';
+}
+
